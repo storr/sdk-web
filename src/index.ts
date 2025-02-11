ed12c59d6943f8b023c4b855db230b412a0a0be1
Tyler Jones <tyler@trybadge.com>
2025-02-11 10:49:46 -0800

    feat: add config passing and feature enablement

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export type {BadgeSdk} from "./sdk.ts";
 export {
   embedTemplatePage,
   type TemplateOptions,
+  type TemplateEmbedFeatures,
 } from "./calls/embedTemplatePage.ts";
 export {makeSdk, type SdkOptions} from "./calls/makeSdk.ts";
